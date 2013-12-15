9985f1affba2cb6596f881d9160efcde4894c463
Bjoern <bjoern@debian>
2013-12-15 22:45:15 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Projektmanagement::Application.config.secret_key_base = '68b5dad14404613976f2e2980b4ab7758583911fa8be918c5e2eea2c9e41f7d77b0ad6a3c251f0ccf567406d34b60bd00925adfb80bcf5d9c7aa7c4c0bd185dc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Projektmanagement::Application.config.session_store :cookie_store, key: '_projektmanagement_session'
